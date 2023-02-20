426c33a3e93ffa3177522607588e4ec3ce39e044
mveery <mariajosech1006@gmail.com>
2023-02-20 19:00:47 -0300

    update 20/02

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.register_blueprint(views, url_prefix="/views")
 def home():
     return render_template("home.html")
 
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
 
--- views.py
+++ views.py
@@ -3,12 +3,9 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html", name="Mary")
+    return render_template("views.html", name="Mary")
+
 
 @views.route("/profile/<username>")
 def profile(username):
-    return render_template("index.html", name=username)
-
-@views.route("/go-to-home")
-def go_to_home():
-    return redirect(url_for("views.home"))
+    return render_template("home.html", name=username)
